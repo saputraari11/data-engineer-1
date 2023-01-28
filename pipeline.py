cbbf3e1050c47586b31179e754d642668f40a154
saputraari11 <saputra_ari_28rpl@student.smktelkom-mlg.sch.id>
2023-01-28 13:21:20 +0700

    project: belajar buat ingestion

---
+++ pipeline.py
@@ -0,0 +1,9 @@
+import pandas 
+
+import sys 
+
+print(sys.argv)
+
+date = sys.argv[1]
+
+print("Job Finished At {date}".format(date=date))
---
+++ pythonArgumen.py
@@ -0,0 +1,12 @@
+import argparse
+
+args = argparse.ArgumentParser()
+
+args.add_argument("-c","--port",help="port number")
+args.add_argument("-s","--hostname",help="hostname database")
+args.add_argument("-u","--username",help="username database")
+args.add_argument("-p","--password",help="password database")
+
+args = args.parse_args()
+
+print("this database is {hostname} with port {port} whose {username} and the password is {password}".format(hostname=args.hostname,port=args.port,username=args.username,password=args.password))
